a9e4bb3af1a634eb48ddb0154ddf92e100802e7e
Rob <bobs-n@hotmail.com>
2021-12-31 10:42:38 +0000

    Inital Auth Setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,57 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
-const App = () => {
-    return (
-        <div>
-            <Routes>
-                <Route exact path="/post" element={<CommentBox />} />
-            <   Route exact path="/" element={<CommentList />} />
-            </Routes>
-        </div>
-    )
-}
+class App extends Component {
+    renderButton(){
+        if(this.props.auth){
+            return(
+                <button>
+                    Sign Out
+                </button>
+            )
+        } else {
+            return (
+                <button>
+                    Sign In
+                </button>
+            )
+        }
+    };
 
-export default App
+    renderHeader(){
+        return (
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/posts">Post a Comment</Link>
+                </li>
+                <li>
+                    {this.renderButton()}
+                </li>
+            </ul>
+        )
+    };
+
+    render(){
+        return (
+            <div>
+                {this.renderHeader()}
+                <Routes>
+                    <Route exact path="/post" element={<CommentBox />} />
+                <   Route exact path="/" element={<CommentList />} />
+                </Routes>
+            </div>
+        )
+    }
+};
+
+function mapStateToProps(state){
+    return { auth: state.auth }
+};
+
+export default connect(mapStateToProps)(App);
