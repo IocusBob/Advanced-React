20c8b1fb8ca70912f89582d48af30504e6814d4c
Rob <bobs-n@hotmail.com>
2021-12-31 10:51:06 +0000

    Integrated action to change auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
-import { Route, Routes, Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import CommentBox from 'components/CommentBox'
-import CommentList from 'components/CommentList'
+import React, { Component } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
 class App extends Component {
     renderButton(){
         if(this.props.auth){
             return(
-                <button>
+                <button onClick={() => this.props.changeAuth(false)}>
                     Sign Out
                 </button>
             )
         } else {
             return (
-                <button>
+                <button onClick={() => this.props.changeAuth(true)}>
                     Sign In
                 </button>
             )
@@ -28,7 +29,7 @@ class App extends Component {
                     <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/posts">Post a Comment</Link>
+                    <Link to="/post">Post a Comment</Link>
                 </li>
                 <li>
                     {this.renderButton()}
@@ -54,4 +55,4 @@ function mapStateToProps(state){
     return { auth: state.auth }
 };
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, actions)(App);
