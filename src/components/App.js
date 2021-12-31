3ee93c74ebeb0913e827ec679184426be5c78115
Rob <bobs-n@hotmail.com>
2021-12-31 10:29:08 +0000

    Routing between components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
 const App = () => {
     return (
         <div>
-            <CommentBox />
-            <CommentList />
+            <Routes>
+                <Route exact path="/post" element={<CommentBox />} />
+            <   Route exact path="/" element={<CommentList />} />
+            </Routes>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Root from 'Root';
 import App from "components/App";
 
 ReactDOM.render(
     <Root>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="*" element={<App />} />
+            </Routes>
+        </BrowserRouter>
     </Root>,
     document.querySelector('#root')
 )
